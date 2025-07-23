5d6ac6f875a48e3ea0aac7100f771a710396c605
etk7 <bb38123013@ms.nagasaki-u.ac.jp>
2025-07-24 04:45:07 +0900

    Update models.py

--- models.py
+++ models.py
@@ -22,6 +22,8 @@ class FriendRequest(db.Model):
     to_user = db.relationship('User', foreign_keys=[to_user_id])
 
 class Achievement(db.Model):
+    __tablename__ = 'achievement'
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     date = db.Column(db.String(10), nullable=False)
